b567b50b626841e1fc902bcf86f250b48324d2c2
JuanMolfese <juanjosemolfese@gmail.com>
2022-09-15 18:37:06 -0300

    se agregan clases Estudiante y Carrera

--- TP2/src/Estructura/Carrera.java
+++ TP2/src/Estructura/Carrera.java
@@ -1,5 +1,38 @@
 package Estructura;
 
 public class Carrera {
+	private int id;
+	private String nombre;
+	private int duracion;
+	
+	private Carrera(String nombre, int duracion) {	
+		//id (autoincremental automatic)
+		this.nombre = nombre;
+		this.duracion = duracion;
+	}
+
+	public int getId_carrera() {
+		return id;
+	}
+
+	public void setId_carrera(int id_carrera) {
+		this.id = id_carrera;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
 
 }
--- TP2/src/Estructura/Estudiante.java
+++ TP2/src/Estructura/Estudiante.java
@@ -1,5 +1,86 @@
 package Estructura;
 
+import java.util.List;
+
 public class Estudiante {
+
+	private int id;
+	private int num_libreta;
+	private String nombres;
+	private String apellido;
+	private int edad;
+	private String genero;
+	private int dni;
+	private String ciudadResidencia;
+	private List<Carrera> carreras;
+	
+	private Estudiante(String nombres, String apellido, int edad, String genero, int dni,String ciudadResidencia) {
+		
+		//id (autoincremental automatic)
+		this.nombres = nombres;
+		this.apellido = apellido;
+		this.edad = edad;
+		this.genero = genero;
+		this.dni = dni;
+		this.ciudadResidencia = ciudadResidencia;
+		//this.carreras = new List<Carrera>();
+	}
+
+	public int getNum_libreta() {
+		return id;
+	}
+
+	public void setNum_libreta(int num_libreta) {
+		this.id = num_libreta;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public int getDni() {
+		return dni;
+	}
+
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+	public String getCiudadResidencia() {
+		return ciudadResidencia;
+	}
+
+	public void setCiudadResidencia(String ciudadResidencia) {
+		this.ciudadResidencia = ciudadResidencia;
+	}
+		
 
 }

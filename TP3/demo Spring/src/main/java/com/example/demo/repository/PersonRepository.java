710e15d2cf08b9729f782f91e80f28624598b171
cattsas <cattsas1980@gmail.com>
2022-10-08 11:11:21 -0300

    Implementamos findAllBySurname y findAllByName

--- TP3/demo Spring/src/main/java/com/example/demo/repository/PersonRepository.java
+++ TP3/demo Spring/src/main/java/com/example/demo/repository/PersonRepository.java
@@ -4,12 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.model.Person;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface PersonRepository  extends JpaRepository<Person, Long> {
 	@Query("SELECT t FROM Person t WHERE  t.surname= :surname")
-	public List <Person> findAllBySurname(String surname);
+	public List <Person> findAllBySurname(@Param("surname") String surname);
 	
 	@Query("SELECT t FROM Person t WHERE t.name= :name")
-	public List <Person> findAllByName(String name);
+	public List <Person> findAllByName(@Param("name") String name);
 }

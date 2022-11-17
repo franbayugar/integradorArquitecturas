35d290b2cfd5dd40fd343246da3bc3e3508530af
fede <fdcrespi@gmail.com>
2022-11-17 20:11:20 -0300

    Se agrego JWT a Cliente

--- TP5/Cliente/src/main/java/com/client/ClientApplication.java
+++ TP5/Cliente/src/main/java/com/client/ClientApplication.java
@@ -1,7 +1,14 @@
 package com.client;
 
+import com.client.security.JWTAuthorizationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SpringBootApplication
 public class ClientApplication {
@@ -9,5 +16,21 @@ public class ClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
     }
+    @EnableWebSecurity
+    @Configuration
+    static
+    class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable()
+                    .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.POST, "/user").permitAll()
+                    .anyRequest().authenticated();
+        }
+
+    }
+
 
 }
